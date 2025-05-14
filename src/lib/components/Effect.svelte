970cb6a2ebfbf1cd993d752db741254ad34ec343
millionaiyer <surajsaiyer@gmail.com>
2025-05-14 14:38:23 +0530

    effect and effect.pre

--- src/lib/components/Effect.svelte
+++ src/lib/components/Effect.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
-	import { untrack } from 'svelte';
+	import { tick, untrack } from 'svelte';
 
 	let randomNumber = $state(Math.floor(Math.random() * 10));
 	// let doubleRandomNumber = $state();
 	let doubleRandomNumber = $derived(randomNumber * 2);
 
 	let history: number[] = $state([untrack(() => randomNumber)]);
+	let historyPTag: HTMLParagraphElement;
 
-	// $effect(() => {
-	// 	// doubleRandomNumber = randomNumber * 2;
-	// 	randomNumber;
-	// 	untrack(() => {
-	// 		history.push(randomNumber);
-	// 	});
-	// });
+	$effect.pre(() => {
+		history.length;
+		console.log('$effect.pre', historyPTag?.innerText);
+		tick().then(() => {
+			console.log('After tick $effect.pre', historyPTag?.innerText);
+		});
+		return () => {
+			console.log('clean up pre');
+		};
+	});
+
+	$effect(() => {
+		history.length;
+		console.log('$effect', historyPTag.innerText);
+		return () => {
+			console.log('clean up effect');
+		};
+	});
 </script>
 
 <h3>The random number is {randomNumber}</h3>
 <h3>Double random number is {doubleRandomNumber}</h3>
-<p>history {history}</p>
+<p bind:this={historyPTag}>history {history}</p>
 
 <button
 	onclick={() => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
 	import Effect from '$lib/components/Effect.svelte';
-	import StateAndDerived from '$lib/components/StateAndDerived.svelte';
 </script>
 
-<StateAndDerived />
-<hr />
 <h1>Effect</h1>
 <Effect />

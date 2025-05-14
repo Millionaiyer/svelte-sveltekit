469a46362e0cc94599dfd63a19f8b491eccda98d
millionaiyer <surajsaiyer@gmail.com>
2025-05-14 14:03:47 +0530

    how to not use effect

---
+++ src/lib/components/Effect.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { untrack } from 'svelte';
+
+	let randomNumber = $state(Math.floor(Math.random() * 10));
+	// let doubleRandomNumber = $state();
+	let doubleRandomNumber = $derived(randomNumber * 2);
+
+	let history: number[] = $state([untrack(() => randomNumber)]);
+
+	// $effect(() => {
+	// 	// doubleRandomNumber = randomNumber * 2;
+	// 	randomNumber;
+	// 	untrack(() => {
+	// 		history.push(randomNumber);
+	// 	});
+	// });
+</script>
+
+<h3>The random number is {randomNumber}</h3>
+<h3>Double random number is {doubleRandomNumber}</h3>
+<p>history {history}</p>
+
+<button
+	onclick={() => {
+		randomNumber = Math.floor(Math.random() * 10);
+		history.push(randomNumber);
+		console.log(randomNumber, doubleRandomNumber);
+	}}>Generate</button
+>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Effect from '$lib/components/Effect.svelte';
+	import StateAndDerived from '$lib/components/StateAndDerived.svelte';
+</script>
+
+<StateAndDerived />
+<hr />
+<h1>Effect</h1>
+<Effect />

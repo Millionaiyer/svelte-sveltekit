b52dbf37ef876ffd7bf3741ac9345840372b61df
millionaiyer <surajsaiyer@gmail.com>
2025-05-16 11:11:01 +0530

    clean code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<script>
-	import Counter from '$lib/components/Counter.svelte';
-	import Effect from '$lib/components/Effect.svelte';
-</script>
-
-<h1>Effect</h1>
-<!-- <Effect /> -->
-<Counter />
-
-<h1>Hello and welcome to my site!</h1>
-<a href="/about">About my site</a>
+<h1>hello world</h1>

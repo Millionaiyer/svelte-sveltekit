da902a985b2d51a0741a75abed55b3a6a7fc09c8
millionaiyer <surajsaiyer@gmail.com>
2025-05-21 12:45:55 +0000

    feat: :sparkles: snippets introduction

---
+++ src/lib/Button.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	let isLeftHovered = $state(false);
+
+	type Props = HTMLButtonAttributes & {
+		left?: Snippet<[boolean]>;
+		right?: Snippet;
+		children: Snippet<[boolean]>;
+		size?: 'sm' | 'lg';
+		shadow?: boolean;
+		onlefthover?: () => void;
+	};
+	let {
+		left,
+		right,
+		size = 'sm',
+		shadow = false,
+		children,
+		onlefthover,
+		...props
+	}: Props = $props();
+</script>
+
+<button class:sm={size == 'sm'} class:lg={size == 'lg'} class:shadow {...props}>
+	{#if left}
+		<div
+			role="presentation"
+			class="left-content"
+			onmouseenter={() => {
+				onlefthover?.();
+				isLeftHovered = true;
+			}}
+			onmouseleave={() => {
+				isLeftHovered = false;
+			}}
+		>
+			{@render left(isLeftHovered)}
+		</div>
+	{/if}
+	{@render children(isLeftHovered)}
+	{#if right}
+		<div class="right-content">
+			{@render right()}
+		</div>
+	{/if}
+</button>
+
+<style lang="scss">
+	button {
+		border: none;
+		background-color: var(--buttonBgColor, #ff3e00);
+		color: var(--buttonTextColor, #ffffff);
+		padding: 0 20px;
+		height: 45px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
+		&:hover {
+			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+		}
+		&:active {
+			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+		}
+		&.sm {
+			height: 45px;
+		}
+		&.lg {
+			height: 55px;
+			font-size: 20px;
+		}
+		&.shadow {
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
+		}
+		.left-content {
+			margin-inline-end: 10px;
+		}
+		.right-content {
+			margin-inline-start: 10px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
+<script lang='ts'>
+	import Button from '$lib/Button.svelte';
+	import { AlarmCheck, Calendar, Search } from '@lucide/svelte';
+
+</script>
 <h1>hello world</h1>
+<!-- 
+{#snippet leftIcon(isHovered:boolean)}
+{#if isHovered}
+<Search/>
+{:else}
+<AlarmCheck/>
+{/if}
+
+{/snippet}
+
+{#snippet rightIcon()}
+<Calendar/>
+{/snippet}
+
+<Button left={leftIcon} right={rightIcon} size='sm' >click me</Button> -->
+
+
+<Button size="lg" disabled>
+	{#snippet left(isHovered)}
+		{#if isHovered}
+			<Search />
+		{:else}
+			<AlarmCheck />
+		{/if}
+	{/snippet}
+	Text
+	{#snippet right()}
+		<AlarmCheck />
+	{/snippet}
+</Button>
